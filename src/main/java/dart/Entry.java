d8dedd1a719c2de6336add7048eacb37b35e7f96
rzstead <rzstead@gmail.com>
2018-06-01 10:03:08 -0600

    added cors

--- src/main/java/dart/Entry.java
+++ src/main/java/dart/Entry.java
@@ -1,22 +1,35 @@
 package dart;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan(basePackages="controllers")
-@EntityScan(basePackages="models")
-@EnableJpaRepositories(basePackages="repos")
+@ComponentScan(basePackages = "controllers")
+@EntityScan(basePackages = "models")
+@EnableJpaRepositories(basePackages = "repos")
 public class Entry extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Entry.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 }
